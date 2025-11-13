e7db16db8fa08b68b2b43d3482decfd6d0a18bcc
TRAM-HUONG <1250080066@sv.hcmunre.edu.vn>
2025-11-13 18:32:55 +0700

    Add files via upload

--- src/main/java/bancaycanh/WebsiteBanCayCanhApplication.java
+++ src/main/java/bancaycanh/WebsiteBanCayCanhApplication.java
@@ -1,14 +1,16 @@
+package bancaycanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 @SpringBootApplication
-@ComponentScan(basePackages = {"controller"})
-@EnableJpaRepositories(basePackages = {"repository"})
-@EntityScan(basePackages = {"model"})
-public class WebsiteBanCayCanhApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(WebsiteBanCayCanhApplication.class);
-    }
-
+@ComponentScan(basePackages = { "controller" })
+@EnableJpaRepositories(basePackages = { "repository" })
+@EntityScan(basePackages = { "model" })
+public class WebsiteBanCayCanhApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebsiteBanCayCanhApplication.class, args);
     }
