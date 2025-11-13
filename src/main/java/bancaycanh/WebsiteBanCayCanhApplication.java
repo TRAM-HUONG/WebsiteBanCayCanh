bc79678f902f0db4ff21d4106edbc79a3c30f8a3
TRAM-HUONG <1250080066@sv.hcmunre.edu.vn>
2025-11-13 16:41:33 +0700

    Update WebsiteBanCayCanhApplication.java

--- src/main/java/bancaycanh/WebsiteBanCayCanhApplication.java
+++ src/main/java/bancaycanh/WebsiteBanCayCanhApplication.java
@@ -2,6 +2,8 @@ package bancaycanh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,7 +12,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = { "controller" })
 @EnableJpaRepositories(basePackages = { "repository" })
 @EntityScan(basePackages = { "model" })
-public class WebsiteBanCayCanhApplication {
+public class WebsiteBanCayCanhApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WebsiteBanCayCanhApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(WebsiteBanCayCanhApplication.class, args);
     }
